eb482f6466e1271d09a81eb3b42045391e62e873
Steven Chang <steven@discourse.org>
2026-08-07 21:05:07 +0000

    DEV: resolve deprecations

--- javascripts/discourse/api-initializers/created-date.js
+++ javascripts/discourse/api-initializers/created-date.js
@@ -53,11 +53,8 @@ export default apiInitializer("1.38.0", (api) => {
     });
   }
 
-  api.modifyClass("model:topic", {
-    pluginId: "created-date",
-    get createdAtTitle() {
-      return i18n("topic.created_at", { date: longDate(this.createdAt) });
-    },
+  api.addModelGetter("topic", "createdAtTitle", function () {
+    return i18n("topic.created_at", { date: longDate(this.createdAt) });
   });
 
   api.registerValueTransformer(
--- javascripts/discourse/components/created-date-item.gjs
+++ javascripts/discourse/components/created-date-item.gjs
@@ -1,5 +1,5 @@
 import Component from "@glimmer/component";
-import { getOwner } from "@ember/application";
+import { getOwner } from "@ember/owner";
 import { service } from "@ember/service";
 import { htmlSafe } from "@ember/template";
 import PluginOutlet from "discourse/components/plugin-outlet";
